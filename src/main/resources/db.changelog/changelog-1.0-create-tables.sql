f5979d83201d91820d65cdcbc0637d6f6cc0bc82
m1crovolnovka <denisdevyaltovski@gmail.com>
2025-12-26 03:44:26 +0300

    create liquibase schema

---
+++ src/main/resources/db.changelog/changelog-1.0-create-tables.sql
@@ -0,0 +1,14 @@
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+CREATE TABLE payments (
+                          id             UUID PRIMARY KEY,
+                          order_id       UUID NOT NULL,
+                          user_id        UUID NOT NULL,
+                          status         VARCHAR(50) NOT NULL,
+                          payment_amount NUMERIC(10,2) NOT NULL,
+                          timestamp      TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE INDEX idx_payments_order_id ON payments(order_id);
+CREATE INDEX idx_payments_user_id ON payments(user_id);
